64c4dd71ce7783ae755d71e9f29468bb8c27c825
Rae J. Khan <raekhan18@gmail.com>
2022-11-10 12:28:29 -0500

    Finished section 1, the hero editor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,20 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
+  // selector is a pattern of elements that tells the browser which HTML elements should be selected
+  // to have CSS elements
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
 }
